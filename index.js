669328a4354fd08858b37e668f8c02034a2ab069
Senyo Dedoo <themrdee@Senyos-MacBook-Air.local>
2023-12-12 19:48:47 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const dns = require("dns");
+const urlparser = require("url");
+
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db('urlshortener');
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +23,28 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+    if (!address) {
+      res.json({error: 'invalid url'});
+    } else {
+       const urlCount = await urls.countDocuments({});
+       const urlDoc = {
+         url: url,
+         short_url: urlCount
+       }
+
+      const result = await urls.insertOne(urlDoc); 
+      res.json({ original_url: url, short_url: urlCount })
+    }
+  });
+});
+
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const short_url = req.params.short_url;
+  const urlDoc = await urls.findOne({short_url: +short_url});
+  res.redirect(urlDoc.url);
 });
 
 app.listen(port, function() {
